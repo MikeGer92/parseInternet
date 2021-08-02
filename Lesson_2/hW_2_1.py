6149718d16f52d763ba7e04ceec6abb01de06e87
MikeGer <mikeman@inbox.ru>
2021-08-02 13:26:22 +0300

    added full parse for HH

--- Lesson_2/hW_2_1.py
+++ Lesson_2/hW_2_1.py
@@ -10,8 +10,8 @@ main_list = []
 def get_last_page_hh(par, head, adr):
     response = requests.get(adr+'/search/vacancy', params=par, headers=head)
     soup = bs(response.text, 'html.parser')
-    last_page = soup.find_all('span', attrs={'class': 'pager-item-not-in-short-range'})
-    last_page_num = int(last_page.split(';')[-1][5:7])+1
+    last_page = soup.find_all('span', attrs={'class': 'pager-item-not-in-short-range'})[-1].text
+    last_page_num = int(last_page)
     return last_page_num
 
 
@@ -26,33 +26,33 @@ def get_parse_hh(par, head, adr):
         if vacancy_salary_tag:
             vacancy_salary_tag = vacancy_salary_tag.getText().replace('\u202f', '').split(' ')
             if 'от' in vacancy_salary_tag:
-                vacancy_dic['мин. З/П'] = (f" {vacancy_salary_tag[1]} {vacancy_salary_tag[2]}")
+                vacancy_dic['мин. З/П'] = (f"{vacancy_salary_tag[1]} {vacancy_salary_tag[2]}")
             elif 'до' in vacancy_salary_tag:
-                vacancy_dic['макс. З/П'] = (f" {vacancy_salary_tag[1]} {vacancy_salary_tag[2]}")
+                vacancy_dic['макс. З/П'] = (f"{vacancy_salary_tag[1]} {vacancy_salary_tag[2]}")
             else:
-                vacancy_dic['З/П'] = (f" от {vacancy_salary_tag[0]} до {vacancy_salary_tag[2]} {vacancy_salary_tag[3]}")
+                vacancy_dic['З/П'] = (f" от{vacancy_salary_tag[0]} до {vacancy_salary_tag[2]} {vacancy_salary_tag[3]}")
         else:
-            vacancy_dic['З/П'] = (f" не указана")
+            vacancy_dic['З/П'] = (f"не указана")
 
-        vacancy_dic['name'] = vacancy_name
-        # vacancy_dic['salary'] = vacancy_salary_tag
+        vacancy_dic['Вакансия'] = vacancy_name
         main_list.append(vacancy_dic)
     pprint(main_list)
 
-# vacancy_name = input('Введите название вакансии, которую необходимо найти:_')
+# vacancy_name = input('Введите название вакансии, которую необходимо найти:_') для поиска заданной пользователем вакансии
 # params_hh['text'] = vacancy_name
 def full_parse_hh(*args, **kwargs):
     l_p_n = get_last_page_hh(*args, **kwargs)
-    print(l_p_n)
+    print(f'Найдено {l_p_n} страниц с вакансиями:')
     for i in range(l_p_n):
+        print(f'Страница {i + 1}:')
         params_hh['page'] = i
         get_parse_hh(*args, **kwargs)
-    print(len(main_list))
-    print(main_list)
 
-# full_parse_hh(params_hh, headers, url_hh)
-# print(get_last_page_hh(params_hh, headers, url_hh))
-get_parse_hh(params_hh, headers, url_hh)
+
+# get_parse_hh(params_hh, headers, url_hh) - для тестового парсинга первой страницы
+get_last_page_hh(params_hh, headers, url_hh) #- для печати количества найденных страниц с вакансией
+full_parse_hh(params_hh, headers, url_hh)
+
 
 
 

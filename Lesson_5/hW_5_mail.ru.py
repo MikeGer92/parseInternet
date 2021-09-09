610777a0dfdb4f56bf1f3b94862b5fb514d17473
MikeGer <mikeman@inbox.ru>
2021-09-09 13:02:21 +0300

    added mail.ru

--- Lesson_5/hW_5_mail.ru.py
+++ Lesson_5/hW_5_mail.ru.py
@@ -1,10 +1,12 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from variables import TEST_LOGIN, TEST_PASSW, TEST_LOGIN_MAIL, TEST_MAIL_PASSW
+from variables import TEST_LOGIN, TEST_PASSW, TEST_LOGIN_MAIL, TEST_MAIL_PASSW, LOGIN, PASSWORD
 
 chrome_options = Options()
 chrome_options.add_argument('start-maximized')
@@ -15,23 +17,28 @@ url = 'https://mail.ru'
 driver.get(url)
 
 login = driver.find_element_by_name('login')
-login.send_keys(TEST_LOGIN_MAIL)
+login.send_keys(LOGIN)
 enter_passw = driver.find_element_by_xpath("//button[@data-testid='enter-password']")
 enter_passw.click()
-passw = driver.find_element_by_xpath("//input[@class='password-input svelte-1eyrl7y']")
+passw = driver.find_element_by_xpath("//input[@class='password-input svelte-1tib0qz']")
 time.sleep(3)
-passw.send_keys(TEST_MAIL_PASSW)
+passw.send_keys(PASSWORD)
 
 passw.send_keys(Keys.ENTER)
-time.sleep(10)
-letters = driver.find_elements_by_class_name('llc__content')
-actions = ActionChains(driver)
-actions.move_to_element(letters[-1])
-actions.perform()
-time.sleep(3)
-letters_links_list = []
-letters_links = driver.find_elements_by_xpath("//a[contains(@class, 'js-tooltip-direction_letter-bottom')]")
-for link in letters_links:
-    letters_links_list.append(link.get_attribute('href'))
-time.sleep(2)
+list_wait = WebDriverWait(driver, 15)
+list_wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'js-tooltip-direction_letter-bottom')))
+letters_links_list = set()
+
+while True:
+    letters_links_list_len = len(letters_links_list)
+    letters_links = driver.find_elements_by_class_name('js-tooltip-direction_letter-bottom')
+    for link in letters_links:
+        letters_links_list.add(link.get_attribute('href'))
+    actions = ActionChains(driver)
+    actions.move_to_element(letters_links[-1])
+    actions.perform()
+    if len(letters_links_list) == letters_links_list_len:
+        break
+
+print (len(letters_links))
 
